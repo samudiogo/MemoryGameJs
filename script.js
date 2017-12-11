5f9c8b087b2dd343c3d14e70f75c259d83becf87
Samuel Jesus <samuel@solidcon.local>
2017-12-11 13:45:51 -0200

    initial

---
+++ script.js
@@ -0,0 +1,9 @@
+(function (){
+var imagens = ['img/facebook.png','img/android.png','img/chrome.png','img/firefox.png','img/html5.png','img/googleplus.png','img/twitter.png','img/windows.png','img/cross.png'];
+
+
+
+
+})();
+
+app.inicio();
